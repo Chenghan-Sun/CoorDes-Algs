1e8f03a92b57e3e168d07a46cedefeb6083432b4
Chenghan Sun <sunchenghan1995@gmail.com>
2020-06-04 02:04:16 -0700

    quadratic ver.

--- Randomized_CD_Alg1.R
+++ Randomized_CD_Alg1.R
@@ -16,29 +16,13 @@ picks = randperm(n)
 xs[picks[1:s]] = 100*randn(s, 1)
 b = A%*%xs
 
-# Define the objective function: Lasso
-lasso_obj = function(y, yhat, xk, lamb){
-  # 1 / (2 * nobs) RSS + lambda * penalty
-  # fun_val = 0.5 * sum((y-yhat)^2) + lam*sum(abs(betat))
-  fun_val = 0.5*norm((y - yhat), "2")^2 + lamb*norm(xk, "1")
+# Define the objective function
+quadratic_obj = function(y, yhat){
+  fun_val = 0.5*norm((y - yhat), "2")^2
   return(fun_val)
 }
 
-# Closed form solution of lasso problem: Soft-Threshold
-shrink_operator = function(b, lamb) {
-  flag = max(0, b-lamb) - max(0, -b-lamb)
-  return(flag)
-}
-
 # Experiment Setup
-# initialize x 
-xk = zeros(n, 1)
-
-# Given value of lamba
-lamb = 1
-
-# initialize gradient vector
-gd = zeros(n, 1)
 
 # initialize fixed CGD step size alpha
 alpha = 0.001
@@ -48,39 +32,46 @@ alpha = 0.001
 # set k as the counter
 k = 1
 cr = c(1)
-fxopt = 1/2*(norm((A%*%xs - b), "2"))^2 + lamb*norm(xs, "1");
 
 # Initialize index iter_k (apply for both cyclic and randomized rules)
 iter_k = 1 
 
 # Coordinate Descent method
 RCDM = function(A, b, xk, xs, cr, fxopt, iter_k, 
-                lamb=1, alpha=0.001, tol=10^-3, maxIter=30000, rule="cyclic") {
-  # iter_k = ik
+                alpha=0.001, tol=10^-2, maxIter=10^7, rule="cyclic") {
+  
+  # initialize x 
+  xk = zeros(n, 1)
+  
+  # initialize gradient vector
+  gd =  zeros(n, 1)
+  
   fx = c()
   error = c()
-  while (cr[iter_k] >= tol) {
+  while (cr[k] >= tol) {
     # update the gradient
     u = A%*%xk - b
     A1 = t(A)
-    gd1 = A1[iter_k, ]*u
-    gd2 = shrink_operator(xk[iter_k], lamb)
-    # gd2 = sign(xk[iter_k])*lamb
-    gd[iter_k] = gd1 + gd2
+    gd_k = A1[iter_k, ]%*%u
+    gd[iter_k] = gd_k
     
     # update xk
     xk[iter_k] = xk[iter_k] - alpha*gd[iter_k]
+    # print(xk[iter_k])
     
     # update stopping criterion 
     cr[k+1] = norm(xk-xs, "2") / norm(xs, "2")
+    print(cr[k+1])
+    
+    print(cr[k+1])
+    
+    # update error 
+    fx = c(fx, quadratic_obj(A%*%xk, b))
+    error = c(error, norm((xk - xs), "2"))
+    #print(error[k])
     
     # update k
     k = k+1
-    
-    # update error 
-    fx = c(fx, lasso_obj(A%*%xk, b, xk, lamb))
-    # fx = c(fx, 1/2*(norm(A%*%xk-b, "2"))^2 + lamb*norm(xk, "1"))
-    error = c(error, abs(fx[k] - fxopt))
     
     # update iter_k
     iter_k = mod(iter_k, n)+1
