0d3f05a7f47528baa5d95f8bf8534f21f8cff37c
Chenghan Sun <sunchenghan1995@gmail.com>
2020-06-04 04:05:54 -0700

    prelim simualtion data with test, review needed

--- Sim_data.R
+++ Sim_data.R
@@ -11,17 +11,26 @@ n = 500
 
 u = randortho(m)  # Generates random orthonormal or unitary matrix of size m
 v = randortho(n)
-s_c = diag(runif(min(m, n)-20, min=30, max=50), nrow=min(m,n), ncol=min(m,n))  # for convexity assumption 
-s_sc = diag(runif(min(m, n), min=30, max=50), nrow=min(m,n), ncol=min(m,n))  # for sigma-strong convexity assumption 
+
+s_c_diag = runif(min(m, n), min=30, max=50)
+s_c_diag[271:300] = 0
+s_c = diag(s_c_diag, nrow=m, ncol=n)  # for convexity assumption 
+s_sc = diag(runif(min(m, n), min=30, max=50), nrow=m, ncol=n)  # for sigma-strong convexity assumption 
 
 # s -> dim(m X n)
-if (m > n) {
-  s = vbind(s, zeros(m-n, n))
-} else {
-  s = cbind(s, zeros(m, n-m))
-}
+# if (m > n) {
+#  s = vbind(s, zeros(m-n, n))
+#} else {
+#  s = cbind(s, zeros(m, n-m))
+#}
 
 # sigular value decomposition
 A_c = u%*%s_c%*%v  # for convexity assumption
 A_sc = u%*%s_sc%*%v  # for sigma-strong convexity assumption 
+
+# test 
+t_scd = svd(A_c)
+t_scd$d
+
+
 
